d672cd26d387b58e65981268672306492458d925
tpetricek <tomas@tomasp.net>
2015-04-27 19:58:11 +0100

    Test things

--- build.fsx
+++ build.fsx
@@ -7,24 +7,24 @@
 open System
 open System.IO
 open Fake
- 
+
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
- 
+
 // Step 2. Use the packages
- 
+
 #r "packages/Suave/lib/net40/Suave.dll"
- 
+
 open Suave // always open suave
 open Suave.Http.Successful // for OK-result
 open Suave.Web // for config
 open Suave.Types
 open System.Net
 
-let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083" 
+let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
 
-let serverConfig = 
+let serverConfig =
     { defaultConfig with
        bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
     }
- 
-startWebServer serverConfig (OK "Hello World! It's Suave.io on Azure Websites. <a href='https://github.com/shanselman/suavebootstrapper'>So easy to setup. Just click Deploy.</a>")
+
+startWebServer serverConfig (OK "Hello World!!!! It's Suave.io on Azure Websites. <a href='https://github.com/shanselman/suavebootstrapper'>So easy to setup. Just click Deploy.</a>")
